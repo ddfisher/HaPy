904db012e14d6b35cbb3d6f493cf9a1671e342f5
ashwins1 <ashwins1@stanford.edu>
2011-12-13 01:04:48 -0800

    Added stuff for the paper (style sheets, sample code. Feel free to run pdflatex -shell-escape paper.tex to see a beautiful minted *.pdf produced before your very eyes)

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import HaPy
 
 mod = HaPy.LoadHaskellModule("./Mod.o")
 print mod.one.toInt()
-print mod.plus(19, mod.one).toInt()
+
+for i in range(3):
+  for j in range(5):
+    print mod.plus(i, j).toInt()
+
 print mod.greeting.toString()
