59fd8342d5250d9a5aab2223945d7826fb2422a6
David Fisher <ddf1991@gmail.com>
2011-12-13 03:10:51 -0800

    changed currying implementation on python side

--- HaPy.py
+++ HaPy.py
@@ -10,55 +10,54 @@ haPy.retrieveDouble.restype = c_double
 haPy.retrieveString.restype = c_char_p
 
 class HaskellObject:
-	def __init__(self, objPtr):
-		self.ptr = objPtr
-	
-	def __call__(self, *args):
-		if args:
-			arg = args[0]
-			applyFun = None
-			if isinstance(arg, int):
-				applyFun = haPy.applyInt
-			elif isinstance(arg, float):
-				applyFun = haPy.applyDouble
-			elif isinstance(arg, bool):
-				applyFun = haPy.applyBool
-			elif isinstance(arg, str):
-				applyFun = haPy.applyString
-			elif isinstance(arg, HaskellObject):
-				applyFun = haPy.applyOpaque
-				arg = arg.ptr
-			return apply(HaskellObject(applyFun(self.ptr, arg)), args[1:])
-		else:
-			return self
+    def __init__(self, objPtr):
+        self.ptr = objPtr
+    
+    def __call__(self, arg, *args):
+        applyFun = None
+        if isinstance(arg, int):
+            applyFun = haPy.applyInt
+        elif isinstance(arg, float):
+            applyFun = haPy.applyDouble
+        elif isinstance(arg, bool):
+            applyFun = haPy.applyBool
+        elif isinstance(arg, str):
+            applyFun = haPy.applyString
+        elif isinstance(arg, HaskellObject):
+            applyFun = haPy.applyOpaque
+            arg = arg.ptr
+        else:
+            raise TypeError("Unknown Object Type!")
+        self.ptr = applyFun(self.ptr, arg)
+        return self(*args) if args else self
 
-	def toInt(self):
-		return haPy.retrieveInt(self.ptr)
-	
-	def toBool(self):
-		return haPy.retrieveBool(self.ptr)
+    def toInt(self):
+        return haPy.retrieveInt(self.ptr)
+    
+    def toBool(self):
+        return haPy.retrieveBool(self.ptr)
 
-	def toDouble(self):
-		return haPy.retrieveDouble(self.ptr)
+    def toDouble(self):
+        return haPy.retrieveDouble(self.ptr)
 
-	def toString(self):
-		return haPy.retrieveString(self.ptr)
+    def toString(self):
+        return haPy.retrieveString(self.ptr)
 
 
 class HaskellModule:
-	def __init__(self, modulePath):
-		self.path = modulePath
+    def __init__(self, modulePath):
+        self.path = modulePath
 
-	def __getattr__(self, name):
-		symPtr = haPy.getSymbol(self.path, name)
-		if symPtr is None:
-			raise AttributeError
+    def __getattr__(self, name):
+        symPtr = haPy.getSymbol(self.path, name)
+        if symPtr is None:
+            raise AttributeError
 
-		return HaskellObject(symPtr)
+        return HaskellObject(symPtr)
 
 def LoadHaskellModule(moduleName):
-	''' moduleName should be of the standard "Data.Int"
-		format using in Haskell import statements
-		So we need to search the package.conf?
-	'''
-	return HaskellModule(moduleName)
+    ''' moduleName should be of the standard "Data.Int"
+        format using in Haskell import statements
+        So we need to search the package.conf?
+    '''
+    return HaskellModule(moduleName)
--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import HaPy
 mod = HaPy.LoadHaskellModule("./Mod.o")
 print mod.one.toInt()
 
-for i in range(3):
-  for j in range(5):
-    print mod.plus(i, j).toInt()
+print mod.plus(1, "cows").toInt()
+
 
 print mod.greeting.toString()
