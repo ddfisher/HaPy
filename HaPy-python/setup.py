c384c84b772c212ffebf06e3e74421ed000f5b1d
David Fisher <ddfisher@dropbox.com>
2014-11-08 17:17:44 -0800

    Bump python lib bugfix version

--- HaPy-python/setup.py
+++ HaPy-python/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='HaPy-ffi',
-      version='0.1.1',
+      version='0.1.2',
       description='Haskell bindings for Python',
       author='David Fisher',
       author_email='ddf1991@gmail.com',
